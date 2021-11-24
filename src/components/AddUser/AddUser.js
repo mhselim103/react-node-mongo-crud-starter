20fa131eaef9780dfe35bfc50d79d431a92b4d9c
Mahmudul Hasan Selim <mhselim103@gmail.com>
2021-11-25 00:05:44 +0600

    first commit

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useRef } from "react";
 
 const AddUser = () => {
-    return (
-        <div>
-            <h2>This is Add User</h2>
-        </div>
-    );
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUser = { name, email };
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          alert("successfully added user");
+          e.target.reset();
+        }
+      });
+  };
+  return (
+    <div>
+      <h2>This is Add User</h2>
+      <form onSubmit={handleSubmit}>
+        <input ref={nameRef} type="text" name="" id="" />
+        <input ref={emailRef} type="email" name="" id="" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default AddUser;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Users = () => {
-    return (
-        <div>
-            <h2>This is Users</h2>
-        </div>
-    );
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h2>Total users : {users.length}</h2>
+      <ul>
+        {users?.map((user) => (
+          <li>
+            {" "}
+            {user.name}:: {user.email} <button>Update</button>{" "}
+            <button>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Users;
