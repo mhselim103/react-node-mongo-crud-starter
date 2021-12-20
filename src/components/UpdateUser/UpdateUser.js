0492e053b5869b75b8bb5b55a424b5f3ed238890
Mahmudul Hasan Selim <mhselim103@gmail.com>
2021-12-20 23:23:59 +0600

    update user from database

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -4,6 +4,33 @@ import { useParams } from "react-router-dom";
 const UpdateUser = () => {
   const [user, setUser] = useState({});
   const { id } = useParams();
+  const nameChange = (e) => {
+    const updatedUser = { ...user };
+    updatedUser.name = e.target.value;
+    setUser(updatedUser);
+  };
+  const emailChange = (e) => {
+    const updatedUser = { ...user };
+    updatedUser.email = e.target.value;
+    setUser(updatedUser);
+  };
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const url = `http://localhost:5000/users/${id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          alert("Updated User Successfully");
+        }
+      });
+  };
   useEffect(() => {
     const url = `http://localhost:5000/users/${id}`;
     fetch(url)
@@ -12,8 +39,15 @@ const UpdateUser = () => {
   }, []);
   return (
     <div>
-      <h1>Update {user.name}</h1>
+      <h1>
+        Name:{user.name} Email:{user.email}
+      </h1>
       <h2>{id}</h2>
+      <form onSubmit={handleUpdate}>
+        <input type="text" onChange={nameChange} value={user.name || ""} />
+        <input type="email" onChange={emailChange} value={user.email || ""} />
+        <input type="submit" value="Update" />
+      </form>
     </div>
   );
 };
